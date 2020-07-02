5a7f6c1d4d919e3fe37fb2bc9349684797a7364d
johnedmartz <64530798+johnedmartz@users.noreply.github.com>
2020-07-02 18:36:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ def read(fname):
       return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-      name='ssos_calib',
+      name='autossos',
       version='1.0',
       author='John Martinez',
       author_email='johnedmf@gmail.com',
       description='Automatic data calibration of SSOS pipeline using Fialbres and GLS',
       packages=find_packages(),
-      package_data={'':['config.ssos_calib', 'config.scamp', 'config.sex', 'default.param']},
+      package_data={'':['config.autossos', 'config.scamp', 'config.sex', 'default.param']},
       license='GPL-3.0',
       keywords='filabres ssos gls',
       long_description=read('README.md'),
-      entry_points={'console_scripts': ['ssos_calib = ssos_calib.__main__:main']},
+      entry_points={'console_scripts': ['autossos = autossos.__main__:main']},
       include_package_data=True
       )
