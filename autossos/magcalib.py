f4d3422b1eef2031b066795e6bf23aac2058e9b1
johnedmartz <64530798+johnedmartz@users.noreply.github.com>
2020-07-22 16:14:10 +0200

    Update magcalib.py

--- autossos/magcalib.py
+++ autossos/magcalib.py
@@ -528,7 +528,6 @@ def getperiod(file, name, iterations):
     data_all = pd.read_csv(file_dir)
     data_ast = data_all[data_all['SKYBOT_NAME'] == name]
     
-    #time = list(data_ast['EPOCH']*365.25*24)
     time = list(data_ast['MID_EXPOSURE_MJD']*24)
     mag = list(data_ast['MAG_CALIB'])
     
